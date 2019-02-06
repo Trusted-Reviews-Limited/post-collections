8a2145e85d2d0df5e8fb6075ee93069048b61aae
Toby Schrapel <toby@tobyschrapel.com>
2019-02-05 19:47:07 -0500

    Add fields from post query widget.

--- widget.php
+++ widget.php
@@ -166,6 +166,7 @@ class Collection_Widget extends WP_Widget {
 		$context                = Timber::get_context();
 		$context['posts']       = Timber::get_posts( $items );
 		$context['image_style'] = $image;
+		$context['instance']    = $instance;
 
 		echo Timber::fetch( 'post-collections/post-collections.twig', $context );
 
@@ -178,6 +179,18 @@ class Collection_Widget extends WP_Widget {
 		$layout = ( isset( $instance['layout'] ) ? $instance['layout'] : 'single' );
 		$image  = ( isset( $instance['image'] ) ? $instance['image'] : '' );
 		$style  = ( isset( $instance['style'] ) ? $instance['style'] : 'style-a' );
+		$ad_position = ( isset( $instance['ad-position'] ) ? $instance['ad-position'] : 0 );
+		$ad_display = ( isset( $instance['ad-display'] ) ? $instance['ad-display'] : 'all' );
+		$teaser_length = ( isset( $instance['teaser_length'] ) ? $instance['teaser_length'] : '' );
+		$show_author = isset( $instance['show_author'] ) ? 'true' : 'false';
+		$show_comment_count = isset( $instance['show_comment_count'] ) ? 'true' : 'false';
+		$show_date = isset( $instance['show_date'] ) ? 'true' : 'false';
+		$show_rating = isset( $instance['show_rating'] ) ? 'true' : 'false';
+		$show_price = isset( $instance['show_price'] ) ? 'true' : 'false';
+		$show_duration = isset( $instance['show_duration'] ) ? 'true' : 'false';
+		$show_start_time = isset( $instance['show_start_time'] ) ? 'true' : 'false';
+		$show_content_type = isset( $instance['show_content_type'] ) ? 'true' : 'false';
+		$show_taxonomy_signpost = isset( $instance['show_taxonomy_signpost'] ) ? 'true' : 'false';
 		?>
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">Title</label>
@@ -239,6 +252,116 @@ class Collection_Widget extends WP_Widget {
 				</select>
 			</p>
 		<?php endif; ?>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'ad-position' ) ); ?>">Ad Position:</label>
+			<select name="<?php echo esc_attr( $this->get_field_name( 'ad-position' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'ad-position' ) ); ?>" class="widefat">
+				<option value="0" <?php selected( $ad_position, 0 ); ?>>No ad</option>
+				<option value="1" <?php selected( $ad_position, 1 ); ?>>1</option>
+				<option value="2" <?php selected( $ad_position, 2 ); ?>>2</option>
+				<option value="3" <?php selected( $ad_position, 3 ); ?>>3</option>
+				<option value="4" <?php selected( $ad_position, 4 ); ?>>4</option>
+				<option value="5" <?php selected( $ad_position, 5 ); ?>>5</option>
+				<option value="6" <?php selected( $ad_position, 6 ); ?>>6</option>
+				<option value="7" <?php selected( $ad_position, 7 ); ?>>7</option>
+				<option value="8" <?php selected( $ad_position, 8 ); ?>>8</option>
+				<option value="9" <?php selected( $ad_position, 9 ); ?>>9</option>
+				<option value="10" <?php selected( $ad_position, 10 ); ?>>10</option>
+			</select>
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'ad-display' ) ); ?>">Display Ad On:</label>
+			<select name="<?php echo esc_attr( $this->get_field_name( 'ad-display' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'ad-display' ) ); ?>" class="widefat">
+				<option value="all" <?php selected( $ad_display, 'all' ); ?>>All Screens</option>
+				<option value="mobile" <?php selected( $ad_display, 'mobile' ); ?>>Mobile Screens</option>
+				<option value="desktop" <?php selected( $ad_display, 'desktop' ); ?>>Desktop Screens</option>
+			</select>
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'teaser_length' ) ); ?>">Teaser Length (words):</label>
+			<input type="text" name="<?php echo esc_attr( $this->get_field_name( 'teaser_length' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'teaser_length' ) ); ?>"
+				   value="<?php echo esc_attr( $teaser_length ); ?>" class="widefat block-input">
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_author' ) ); ?>">
+				<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_author' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_author' ) ); ?>"
+					<?php checked( $show_author, 'true' ); ?> class="block-input">
+				Show Author
+			</label>
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_comment_count' ) ); ?>">
+				<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_comment_count' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_comment_count' ) ); ?>"
+					<?php checked( $show_comment_count, 'true' ); ?> class="block-input">
+				Show Comment Count
+			</label>
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_date' ) ); ?>">
+				<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_date' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_date' ) ); ?>"
+					<?php checked( $show_date, 'true' ); ?> class="block-input">
+				Show Date
+			</label>
+		</p>
+
+		<?php if ( post_type_exists( 'review' ) ) : ?>
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_rating' ) ); ?>">
+				<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_rating' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_rating' ) ); ?>"
+					<?php checked( $show_rating, 'true' ); ?> class="block-input">
+				Show Rating
+			</label>
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_price' ) ); ?>">
+				<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_price' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_price' ) ); ?>"
+					<?php checked( $show_price, 'true' ); ?> class="block-input">
+				Show Price
+			</label>
+		</p>
+		<?php endif; ?>
+
+		<?php if ( post_type_exists( 'video' ) ) : ?>
+			<p>
+				<label for="<?php echo esc_attr( $this->get_field_id( 'show_duration' ) ); ?>">
+					<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_duration' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_duration' ) ); ?>"
+						<?php checked( $show_duration, 'true' ); ?> class="block-input">
+					Show Duration
+				</label>
+			</p>
+		<?php endif; ?>
+
+		<?php if ( post_type_exists( 'event' ) ) : ?>
+			<p>
+				<label for="<?php echo esc_attr( $this->get_field_id( 'show_start_time' ) ); ?>">
+					<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_start_time' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_start_time' ) ); ?>"
+						<?php checked( $show_start_time, 'true' ); ?> class="block-input">
+					Show Start Time (Events)
+				</label>
+			</p>
+		<?php endif; ?>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_content_type' ) ); ?>">
+				<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_content_type' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_content_type' ) ); ?>"
+					<?php checked( $show_content_type, 'true' ); ?> class="block-input">
+				Show Content Type
+			</label>
+		</p>
+
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_taxonomy_signpost' ) ); ?>">
+				<input type="checkbox" name="<?php echo esc_attr( $this->get_field_name( 'show_taxonomy_signpost' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'show_taxonomy_signpost' ) ); ?>"
+					<?php checked( $show_taxonomy_signpost, 'true' ); ?> class="block-input">
+				Show Taxonomy Signpost
+			</label>
+		</p>
 
 		<style>
 			.js-post-collection-items-wrapper,
@@ -306,6 +429,20 @@ class Collection_Widget extends WP_Widget {
 		</script>
 
 		<?php
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$new_instance['show_author'] = isset( $new_instance['show_author'] ) ? 'true' : null;
+		$new_instance['show_comment_count'] = isset( $new_instance['show_comment_count'] ) ? 'true' : null;
+		$new_instance['show_date'] = isset( $new_instance['show_date'] ) ? 'true' : null;
+		$new_instance['show_rating'] = isset( $new_instance['show_rating'] ) ? 'true' : null;
+		$new_instance['show_price'] = isset( $new_instance['show_price'] ) ? 'true' : null;
+		$new_instance['show_duration'] = isset( $new_instance['show_duration'] ) ? 'true' : null;
+		$new_instance['show_start_time'] = isset( $new_instance['show_start_time'] ) ? 'true' : null;
+		$new_instance['show_content_type'] = isset( $new_instance['show_content_type'] ) ? 'true' : null;
+		$new_instance['show_taxonomy_signpost'] = isset( $new_instance['show_taxonomy_signpost'] ) ? 'true' : null;
+
+		return $new_instance;
 	}
 }
 
